3d3184d41f2cff62e67b46b62c9fa29529cdec8a
nguyenhieu409 <hieu.nguyen@successsoftware.global>
2022-07-25 15:23:13 +0700

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { SlideComponent } from './components/slide/slide.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule,
